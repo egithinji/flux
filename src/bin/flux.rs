d3566e192b8614d49271a5dde1f8857decf1b0dc
Eric Githinji <ericgithinji@gmail.com>
2020-12-18 07:46:06 +0300

    Fix bug caused when area name was not converted to title case when saving area in the feature collection.

--- src/bin/flux.rs
+++ src/bin/flux.rs
@@ -24,6 +24,7 @@ use flux::pattern_matching::match_location_description;
 use chrono::prelude::*;
 use std::env;
 use flux::config::Config;
+use convert_case::{Case, Casing};
 
 
 #[tokio::main]
@@ -165,7 +166,7 @@ fn process_tweet(tweet: egg_mode::tweet::Tweet) -> Option<Feature> {
                 posted_on: lt_formatted,
                 text: tweet.text.to_owned(),
                 user_id: uid,
-                area: v,
+                area: v.to_case(Case::Title),
             };
 
             //Convert the tweet to a Feature
